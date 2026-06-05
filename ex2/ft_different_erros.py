04dd5ef36f25b2af8b9744e520b93dfd8639abbf
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s1.42lisboa.com>
2026-06-05 11:24:04 +0100

    done

--- ex2/ft_different_erros.py
+++ ex2/ft_different_erros.py
@@ -2,7 +2,7 @@ def garden_operations(operation_number: int) -> None:
     if operation_number == 0:
         int("abc")
     elif operation_number == 1:
-        1 / 0
+        print(1 / 0)
     elif operation_number == 2:
         open("/non/existent/file")
     elif operation_number == 3:
